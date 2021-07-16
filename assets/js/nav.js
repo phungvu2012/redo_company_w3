914916a2558dd625ead713be4f668378ab9f6c5a
phungvu2012 <phungvuminh2012@gmail.com>
2021-07-16 10:41:43 +0700

    fix slide (pre, next)

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,6 +2,8 @@ var navBarIcon = document.querySelector("#header-site .bar-icon");
 var header = document.getElementById("header-site");
 var heightHeader = header.clientHeight;
 
+var navItems = document.querySelectorAll("#nav-menu .nav-list li");
+
 navBarIcon.addEventListener("click", function() {
     var isClosed = header.clientHeight === heightHeader;
     if(isClosed) {
@@ -13,3 +15,9 @@ navBarIcon.addEventListener("click", function() {
         console.log("fail", heightHeader)
     }
 })
+
+for(let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function() {
+        header.style.height = heightHeader + "px";
+    })
+}
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,6 +2,8 @@ var pre = document.getElementById("pre");
 var next = document.getElementById("next");
 var slideList = document.querySelector("#portfolio .slider-list");
 var slideItems = document.querySelectorAll("#portfolio .slider-list li")
+var toogle = document.querySelectorAll(".toogle i");
+
 
 var counter = 1;
 var percent = 100 / slideItems.length; 
@@ -9,13 +11,18 @@ var percent = 100 / slideItems.length;
 // console.log(percent);
 
 slideNumber(counter);
+chooseSlide();
 
 function slideNumber(n = counter) {
+    removeActiveIcon();
+    if (counter === 0) toogle[toogle.length - 1].classList.add("slide-active");
+    else if (counter === slideItems.length - 1) toogle[0].classList.add("slide-active");
+    else toogle[counter-1].classList.add("slide-active");
     slideList.style.transform = "translateX(-" + (n * percent) + "%)";
 }
 
 function nextSlide() {
-    // console.log(counter);
+    console.log(counter);
     if (counter >= slideItems.length - 1) return;
     counter++;
     slideList.style.transition = "transform 0.5s ease-in-out";
@@ -51,5 +58,21 @@ slideList.addEventListener("transitionend", () => {
         slideNumber(counter);
     }
 });
+function chooseSlide() {
+    for(let i = 0; i < toogle.length; i++) {
+        console.log(toogle[i]);
+        toogle[i].addEventListener("click", function() {
+            counter = i + 1;
+            slideList.style.transition = "transform 0.5s ease-in-out";
+            slideNumber(counter);
+        });
+    }
+}
+
+function removeActiveIcon() {
+    for(let i = 0; i < toogle.length; i++) {
+        toogle[i].classList.remove("slide-active");
+    }
+}
 
 autoSlide();
